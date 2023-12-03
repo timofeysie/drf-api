acc53e45122eb784384b0cb194a5b6b087874a69
tim_curchod <timofeyc@hotmail.com>
2023-12-03 19:31:41 +0900

    add some basic tests for the profiles api

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class ProfileListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='adam', password='pass')
+    
+    def test_can_list_profiles(self):
+        response = self.client.get('/profiles/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_get_profile_details(self):
+        response = self.client.get('/profiles/1/')
+        print(response)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_can_get_profile_details_fail(self):
+        response = self.client.get('/profiles/2/')
+        print(response)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
